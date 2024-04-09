d7e858991b3a055479c9836f8ac2849313056f99
JeissonA <110418160+JeissonA@users.noreply.github.com>
2024-04-09 15:47:49 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -26,3 +26,31 @@ window.addEventListener('load', function() {
     var audio = document.getElementById('audio1');
     audio.play();
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Muestra el loader al hacer clic en un enlace
+    document.querySelectorAll('a').forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            var targetSectionId = this.getAttribute('href');
+            var targetSection = document.querySelector(targetSectionId);
+            
+            // Oculta todas las secciones
+            document.querySelectorAll('.section').forEach(function(section) {
+                section.classList.remove('active');
+            });
+
+            // Muestra el loader
+            document.getElementById('loader').style.display = 'block';
+            
+            // Simula una carga (puede ser una petición AJAX, etc.)
+            setTimeout(function() {
+                // Oculta el loader después de un tiempo de espera (simulado)
+                document.getElementById('loader').style.display = 'none';
+                // Muestra la sección destino
+                targetSection.classList.add('active');
+            }, 2000); // Simulando una carga de 2 segundos
+        });
+    });
+});
+
